147641b1bac6f22c3c4aeb39542bfc47c34ca1cf
Mayck Coelho <mayck_coelho@hotmail.com>
2018-04-27 18:24:03 -0300

    Primeira carga.

---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Navigation/Pagina1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina1 : ContentPage
+	{
+		public Pagina1 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarParaPagina2(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pagina2());
+        }
+    }
+}
