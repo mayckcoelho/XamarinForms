1222ff1f7687832c82997016bbf65a5b8657dfeb
Mayck Coelho <mayck_coelho@hotmail.com>
2018-05-10 18:25:22 -0300

    App01 - Concluído

---
+++ Seção 08/App01_ControleXF/App01_ControleXF/App01_ControleXF/Controles/ImagePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_ControleXF.Controles
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ImagePage : ContentPage
+	{
+		public ImagePage ()
+		{
+			InitializeComponent ();
+            //ImageOne.IsLoading
+
+            Image imgUSB = new Image();
+            imgUSB.Source = ImageSource.FromFile(Device.RuntimePlatform == Device.UWP ? "Imagem/usb.jpg" : "usb.jpg");
+
+            Container.Children.Add(imgUSB);
+        }
+	}
+}
