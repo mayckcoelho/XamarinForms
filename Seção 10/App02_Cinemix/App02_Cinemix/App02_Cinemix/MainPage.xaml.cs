31c8632cca8f90a394278abecd75c181ea3e78d0
Mayck Coelho <mayck_coelho@hotmail.com>
2018-05-17 18:10:27 -0300

    App02 - Concluído

---
+++ Seção 10/App02_Cinemix/App02_Cinemix/App02_Cinemix/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App02_Cinemix
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Sinopse.Text = @"Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores.
+Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados.";
+
+        }
+	}
+}
