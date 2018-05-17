b5e0a0a5a1f59398f310f00e8de46453e91ad8d8
Mayck Coelho <mayck_coelho@hotmail.com>
2018-05-17 15:51:06 -0300

    App01 - Concluído

---
+++ Seção 10/App01_Estilo/App01_Estilo/App01_Estilo/Menu/Master.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_Estilo.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Master : MasterDetailPage
+	{
+		public Master ()
+		{
+			InitializeComponent ();
+		}
+
+        private void GoImplicitStylePage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.ImplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void GoExplicitStylePage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.ExplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void GoGlobalStylePage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.GlobalStylePage());
+            IsPresented = false;
+        }
+
+        private void GoInheritStylePage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.InheritStylePage());
+            IsPresented = false;
+        }
+
+        private void GoDynamicStylePage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pagina.DynamicStylePage());
+            IsPresented = false;
+        }
+    }
+}
---
+++ Seção 10/App01_Estilo/App01_Estilo/App01_Estilo/Pagina/DynamicStylePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App01_Estilo.Pagina
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DynamicStylePage : ContentPage
+	{
+		public DynamicStylePage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.Resources["LblColor"] = Color.Orange;
+            this.Resources["Lbl"] = this.Resources["NewLbl"];
+        }
+    }
+}
