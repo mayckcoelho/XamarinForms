ae3d03286725fcadf5ecfad37ad7edbf2ef59cff
Mayck Coelho <mayck_coelho@hotmail.com>
2018-05-30 17:31:14 -0300

    App02 - ListView - Concluído

---
+++ Avançado/App02/App02/App02/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App02
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            ListaFuncionarios.ItemsSource = GetFuncionarios();
+		}
+
+        public class Grupo : List<Funcionario>
+        {
+            public string Titulo { get; set; }
+            public string TituloCurto { get; set; }
+            public string Descricao { get; set; }
+
+            public Grupo(string titulo, string tituloCurto, string descricao)
+            {
+                this.Titulo = titulo;
+                this.TituloCurto = tituloCurto;
+                this.Descricao = descricao;
+            }
+        }
+
+        public class Funcionario
+        {
+            public string Nome { get; set; }
+            public int RankEficiencia{ get; set; }
+            public bool IsObrigatorio { get; set; }
+        }
+
+        private List<Grupo> GetFuncionarios()
+        {
+            return new List<Grupo>()
+            {
+                new Grupo("Presidente", "CEO", "Presidente da Empresa")
+                {
+                    new Funcionario() { Nome = "Mayck", IsObrigatorio = true, RankEficiencia = 8 }
+                },
+                new Grupo("Diretores", "Dir.", "Diretores da Empresa")
+                {
+                    new Funcionario() { Nome = "José", IsObrigatorio = true, RankEficiencia = 8 },
+                    new Funcionario() { Nome = "João", IsObrigatorio = false }
+                },
+                new Grupo("Gerentes", "Ger.", "Gerentes de Departamentos")
+                {
+                    new Funcionario() { Nome = "Maria", IsObrigatorio = true, RankEficiencia = 5 },
+                    new Funcionario() { Nome = "Felipe", IsObrigatorio = false },
+                    new Funcionario() { Nome = "Judas", IsObrigatorio = false }
+                },
+                new Grupo("Colaboradores", "Col.", "Colaboradores da Empresa")
+                {
+                    new Funcionario() { Nome = "Pedro", IsObrigatorio = true, RankEficiencia = 8 },
+                    new Funcionario() { Nome = "Simão", IsObrigatorio = true, RankEficiencia = 8 },
+                    new Funcionario() { Nome = "Danilo", IsObrigatorio = true, RankEficiencia = 8 },
+                    new Funcionario() { Nome = "Jéssica", IsObrigatorio = false },
+                    new Funcionario() { Nome = "Elen", IsObrigatorio = false },
+                    new Funcionario() { Nome = "Débora", IsObrigatorio = false },
+                    new Funcionario() { Nome = "Augusto", IsObrigatorio = false },
+                    new Funcionario() { Nome = "Antonio", IsObrigatorio = false },
+                    new Funcionario() { Nome = "Danilo", IsObrigatorio = true, RankEficiencia = 8 },
+                }
+            };
+        }
+	}
+}
---
+++ Avançado/App02/App02/App02/SeletorTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App02
+{
+    public class SeletorTemplate : DataTemplateSelector
+    {
+        DataTemplate ItemPessoaObrigatoria;
+        DataTemplate ItemPessoaNaoObrigatoria;
+
+        public SeletorTemplate()
+        {
+            ItemPessoaObrigatoria = new DataTemplate(typeof(Templates.ItemPessoaObrigatoriaViewCell));
+            ItemPessoaNaoObrigatoria = new DataTemplate(typeof(Templates.ItemPessoaNaoObrigatoriaViewCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var pessoa = item as MainPage.Funcionario;
+
+            if (pessoa.IsObrigatorio)
+                return ItemPessoaObrigatoria;
+            else
+                return ItemPessoaNaoObrigatoria;
+        }
+    }
+}
