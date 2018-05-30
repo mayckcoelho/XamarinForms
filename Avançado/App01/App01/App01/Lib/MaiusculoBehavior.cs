314332c2be44cec1a5eb35eda669c2f0a66af66d
Mayck Coelho <mayck_coelho@hotmail.com>
2018-05-30 14:31:26 -0300

    App01 - Behaviors e Triggers - Concluído

---
+++ Avançado/App01/App01/App01/Lib/MaiusculoBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App01.Lib
+{
+    public class MaiusculoBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += MaiusculoAction;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= MaiusculoAction;
+        }
+
+        private void MaiusculoAction(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(e.NewTextValue))
+                ((Entry)sender).Text = e.NewTextValue.ToUpper();
+        }
+    }
+}
