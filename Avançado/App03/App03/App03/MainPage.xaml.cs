01314d84f0b80dc4240a062a1a3c139960a470e5
Mayck Coelho <mayck_coelho@hotmail.com>
2018-06-01 10:32:19 -0300

    App03 - Android Nativo - Concluído

---
+++ Avançado/App03/App03/App03/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+#if __ANDROID__
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+using Android.Support.Design.Widget;
+#endif
+
+namespace App03
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+
+#if __ANDROID__
+            TextView labelDroid = new TextView(Forms.Context) { Text = "Eu sou Android Nativo!" };
+            Container.Children.Add(labelDroid);
+
+            FloatingActionButton fab = new FloatingActionButton(Forms.Context);
+            fab.SetImageResource(Android.Resource.Drawable.IcMediaPlay);
+            
+
+            fab.Click += delegate
+            {
+                DisplayAlert("FAB", "FAB Clicado!", "OK");
+            };
+            ContainerAbs.Children.Add(fab);
+#endif
+        }
+    }
+}
