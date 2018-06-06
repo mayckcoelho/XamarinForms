8c7350cb038232eb1228d1162122ae46d08f0623
Mayck Coelho <mayck_coelho@hotmail.com>
2018-06-06 11:00:35 -0300

    App06 - Custom Control Nativo - Concluído

---
+++ Avançado/App06/App06/App06.Android/Controls/CustomBoxViewRenderer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using App06.Controls;
+using App06.Droid.Controls;
+using System.ComponentModel;
+
+[assembly:ExportRenderer(typeof(CustomBoxView), typeof(CustomBoxViewRenderer))]
+namespace App06.Droid.Controls
+{
+    public class CustomBoxViewRenderer : BoxRenderer
+    {
+        public CustomBoxViewRenderer(Context context) : base(context)
+        {
+            SetWillNotDraw(false);
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            base.Draw(canvas);
+
+            var control =(CustomBoxView)Element;
+
+            Paint p = new Paint
+            {
+                StrokeWidth = (float)control.Espessura,
+            };
+
+            canvas.DrawRect(new Rect(0, 0, 200, 200), p);
+
+            p.Color = Android.Graphics.Color.White;
+
+            canvas.DrawLine(100, 0, 100, 200, p);
+
+            canvas.DrawLine(0, 100, 200, 100, p);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            Invalidate();
+        }
+    }
+}
---
+++ Avançado/App06/App06/App06.iOS/Controls/CustomBoxViewRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using App06.Controls;
+using App06.iOS.Controls;
+using CoreGraphics;
+using System.ComponentModel;
+
+[assembly:ExportRenderer(typeof(CustomBoxView), typeof(CustomBoxViewRenderer))]
+namespace App06.iOS.Controls
+{
+    public class CustomBoxViewRenderer : BoxRenderer
+    {
+        public override void Draw(CGRect rect)
+        {
+            //base.Draw(rect);
+            var control = (CustomBoxView)Element;
+
+            using (var context = UIGraphics.GetCurrentContext())
+            {
+                context.SetStrokeColor(new CGColor(0, 0, 0));
+                context.SetLineWidth((float)control.Espessura);
+
+                context.AddRect(new CGRect(0, 0, 200, 200));
+                context.DrawPath(CGPathDrawingMode.Stroke);
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == CustomBoxView.EspessuraProperty.PropertyName)
+            {
+                SetNeedsDisplay();
+            }
+            //base.OnElementPropertyChanged(sender, e);
+        }
+    }
+}
