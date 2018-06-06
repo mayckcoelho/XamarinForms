6ebb60686e962b099cc5ed0273609073a9d39912
Mayck Coelho <mayck_coelho@hotmail.com>
2018-06-06 15:06:33 -0300

    App09 - Multilingual AppToolkit - Concluído

---
+++ Avançado/App09/App09/App09/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App09
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            LblNome.Text = Properties.Resources.LBL_NOME;
+            TxtNome.Placeholder = Properties.Resources.TXT_PH_NOME;
+            BtnSalvar.Text = Properties.Resources.BTN_SALVAR;
+		}
+	}
+}
