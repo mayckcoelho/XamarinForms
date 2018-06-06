255b1ec40c0c4465b4c9c0d2855b421553d94cfb
Mayck Coelho <mayck_coelho@hotmail.com>
2018-06-06 16:57:35 -0300

    App12 - DataAnnotations - Concluído

---
+++ Avançado/App12/App12/App12/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App12.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace App12
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            BtnSalvar.Clicked += delegate
+            {
+                Pessoa pessoa = new Pessoa()
+                {
+                    Nome = TxtNome.Text,
+                    Email = TxtEmail.Text,
+                    CPF = TxtCpf.Text
+                };
+
+                var listaRes = new List<ValidationResult>();
+                var context = new ValidationContext(pessoa);
+                if (!Validator.TryValidateObject(pessoa, context, listaRes, true))
+                {
+                    LblMsg.TextColor = Color.Red;
+                    if (listaRes.Count > 0)
+                    {
+                        string Errors = string.Empty;
+                        listaRes.ForEach(p => Errors += string.Format(p.ErrorMessage + "\n", p.MemberNames));
+                        LblMsg.Text = Errors;
+                    }
+                }
+                else
+                {
+                    LblMsg.TextColor = Color.Green;
+                    LblMsg.Text = "OK!";
+                }
+            };
+		}
+	}
+}
---
+++ Avançado/App12/App12/App12/Model/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace App12.Model
+{
+    public class Pessoa
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo Nome é obrigatório.")]
+        [StringLength(60, MinimumLength = 5)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessageResourceName = nameof(Lang.Mensagem.MSG_01), ErrorMessageResourceType = typeof(Lang.Mensagem))]
+        [EmailAddress(ErrorMessageResourceName = nameof(Lang.Mensagem.MSG_02), ErrorMessageResourceType = typeof(Lang.Mensagem))]
+        public string Email { get; set; }
+        [Required(ErrorMessageResourceName = nameof(Lang.Mensagem.MSG_01), ErrorMessageResourceType = typeof(Lang.Mensagem))]
+        [CPF(ErrorMessageResourceName = nameof(Lang.Mensagem.MSG_02), ErrorMessageResourceType = typeof(Lang.Mensagem))]
+        public string CPF { get; set; }
+    }
+}
